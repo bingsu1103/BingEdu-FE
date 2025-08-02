2f55993a00b628dbf08854ed3a86b49ad9e523ce
An Ngo <ngogiaan11111@gmail.com>
2025-08-02 15:46:01 +0700

    add refresh token in axios config

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -5,24 +5,39 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+const handleRefreshToken = async () => {
+  const res = await instance.post("/v1/api/auth/refresh_token");
+  if (res && res.data) {
+    return res.data.access_token;
+  } else return null;
+};
+
 instance.interceptors.request.use(
-  function (config) {
+  function (config: any) {
     const token = localStorage.getItem("access_token");
     const auth = token ? `Bearer ${token}` : "";
     config.headers["Authorization"] = auth;
     return config;
   },
-  function (error) {
+  function (error: any) {
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
-  function (response) {
+  function (response: any) {
     if (response && response.data) return response.data;
     return response;
   },
-  function (error) {
+  async function (error: any) {
+    if (error.config && error.response && +error.response.status === 401) {
+      const access_token = await handleRefreshToken();
+      if (access_token) {
+        error.config.headers["Authorization"] = `Bearer ${access_token}`;
+        localStorage.setItem("access_token", access_token);
+        return instance.request(error.config);
+      }
+    }
     if (error && error.response && error.response.data)
       return error.response.data;
     return Promise.reject(error);
